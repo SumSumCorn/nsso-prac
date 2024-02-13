cce67ac4643d520b3963b8d1e7ed796f40f2b24e
sumsumcorn <nicecorntea@naver.com>
2024-02-13 22:06:14 +0900

    feat: Initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className='flex h-screen justify-center items-center'>
+      <h1 className='text-7xl'>hello world!</h1>
+    </div>
+  );
+}
